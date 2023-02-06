2138d9910496f691affc58d6727e0c93da9223fb
Norbert Schlüter <=>
2023-02-06 13:26:44 +0100

    Add comment for filters

--- bikeshare.py
+++ bikeshare.py
@@ -8,7 +8,7 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
-
+# This function provides the filtering for cities, months and days
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
